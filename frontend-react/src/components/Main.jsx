51cc4ad0af703dc65eb322220b221c368aea2b71
Akshit Bansal <akshit08082003@gmail.com>
2026-03-18 12:36:11 +0530

    Registration Page Setup

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -1,22 +1,26 @@
 import Button from "./Button";
+import Footer from "./Footer";
+import Header from "./Header";
 
 const Main = () => {
   return (
     <>
-      <div className="container">
-        <div className="p-5 text-center bg-light-dark rounded">
-          <h1 className="text-light">Stock Prediction Portal</h1>
-          <p className="text-light lead">
-            This stock prediction application utilizes machine learning
-            techniques, specifically employing Keras, and LSTM model, integrated
-            within the Django framework. It forecasts future stock prices by
-            analyzing 100-day and 200-day moving averages, essential indicators
-            widely used by stock analysts to inform trading and investment
-            decisions.
-          </p>
-          <Button text="Login" class="btn-outline-info" />
+      
+        <div className="container">
+          <div className="p-5 text-center bg-light-dark rounded">
+            <h1 className="text-light">Stock Prediction Portal</h1>
+            <p className="text-light lead">
+              This stock prediction application utilizes machine learning
+              techniques, specifically employing Keras, and LSTM model, integrated
+              within the Django framework. It forecasts future stock prices by
+              analyzing 100-day and 200-day moving averages, essential indicators
+              widely used by stock analysts to inform trading and investment
+              decisions.
+            </p>
+            <Button text="Login" class="btn-outline-info" />
+          </div>
         </div>
-      </div>
+        
     </>
   );
 };
